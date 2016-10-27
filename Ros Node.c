98b4644d0503c8ce347f76c9296db1f0ce0150eb
Andrew Kamal <kamalandrew55@gmail.com>
2016-10-27 19:34:19 -0400

    Create Ros Node.c

---
+++ Ros Node.c
@@ -0,0 +1,9 @@
+<package>
+  <description brief="BRIEF DESCRIPTION">
+     LONGER DESCRIPTION
+  </description>
+  <author>You/you@willowgarage.com</author>
+  <license>BSD</license>
+  <url>http://www.ros.org/wiki/YOURPACKAGE</url>
+  <depend package="roscpp"/>
+</package>
