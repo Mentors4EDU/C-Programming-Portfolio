3ab0c731112ff3e97b0d4afc5ae13b1c6f877896
Andrew Kamal <kamalandrew55@gmail.com>
2020-07-19 20:13:15 -0400

    Create 2A.c

---
+++ Problems/2A.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+// Andrew was here
+int main()
+{
+   int save1=5,save2=2; //Set the integers and name the save
+   float average; // Average variable
+   int total; // set total for integer
+   total=save1+save2; // Set total given the variables
+   average=total/2.0; // Divide
+   printf("Total of %d and %d = %d",save1,save2,total); // Print numerical as total
+   printf("\nAverage of %d and %d = %.1f",save1,save2,average); // Show the average
+   return 0; // Otherwise return 0 "undefined"
+}
